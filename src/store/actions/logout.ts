5e35f65905b808a341e8cef05cd3904f39809334
Trezy <tre@trezy.com>
2025-08-07 14:01:05 -0500

    ensure all token data is destroyed on logout

--- src/store/actions/logout.ts
+++ src/store/actions/logout.ts
@@ -9,6 +9,7 @@ import * as API from '@/helpers/API'
 export async function logout() {
 	await API.logout()
 
+	localStorage.removeItem('authToken')
 	store.set(() => ({
 		isAuthenticated: false,
 		userProfile: null,
